00f7d4dd00526d064cdbb8a803db65e2f2477dcd
seilixx <mohaillies2020@gmail.com>
2023-09-27 11:25:07 +0100

    new folders

---
+++ frquestions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Quelle est la date de naissance du Prophète Muhammed ?",
+        answer: "Abdullah ibn Abd al-Muttalib and Amina bint Wahb",
+        options: [
+            "Hashim ibn Abd Manaf and Salma bint Amr",
+            "Abdullah ibn Abd al-Muttalib and Amina bint Wahb",
+            "Abu Talib and Fatima bint Asad",
+            "Abdul Muttalib and Halimah bint Abi Dhuayb"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Quelle est l’importance de La Mecque dans la vie du prophète Muhammed ?",
+        answer: "C'est là qu'il est né et a commencé sa mission prophétique.",
+        options: [
+            "C'est là qu'il est né et a commencé sa mission prophétique.",
+            "C'est là qu'il a émigré pour échapper aux persécutions.",
+            "C'est là qu'il a reçu sa première révélation",
+            "C'est là qu'il a écrit le Coran."
+        ]
+    },
+    {
+        numb: 3,
+        question: "Qui était la première épouse du prophète Muhammed ?",
+        answer: "Khadija bint Khuwaylid",
+        options: [
+            " Aisha bint Abi Bakr",
+            "Khadija bint Khuwaylid",
+            "Sawda bint Zam’a",
+            "Hafsa bint Umar"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Quelle est la signification de l’Hégire dans la vie du Prophète Muhammed ?",
+        answer: "Il marque son voyage de La Mecque à Médine.",
+        options: [
+            "Il marque son voyage de Médine à La Mecque.",
+            "Cela marque son voyage de Médine à Jérusalem.",
+            "Cela marque son voyage de La Mecque à Jérusalem.",
+            "Il marque son voyage de La Mecque à Médine."
+        ]
+    },
+    {
+        numb: 5,
+        question: "Quel est le nom complet du Prophète Muhammed ?",
+        answer: "Muhammad ibn Abdullah ibn Abdul-Muttalib",
+        options: [
+            "Muhammad ibn Ali ibn Abu Talib",
+            "Muhammad ibn Umar ibn Khattab",
+            "Muhammad ibn Abu Bakr ibn Uthman",
+            "Muhammad ibn Abdullah ibn Abdul-Muttalib"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Quel était le nom de la mère qui allaitait le prophète Mahomet ?",
+        answer: "Halimah bint Abi Dhuayb",
+        options: [
+            " Khadijah bint Khuwaylid",
+            "Amina bint Wahb",
+            "Fatimah bint Muhammad",
+            "Halimah bint Abi Dhuayb"
+        ]
+    },
+    {
+        numb: 7,
+        question: "Lequel d’entre eux est le fils du prophète Muhammed ?",
+        answer: "Qasim",
+        options: [
+            "Qasim",
+            "Mohamed",
+            "Issa",
+            "Hussein"
+        ]
+    },
+    {
+        numb: 8,
+        question: "Quelle a été la première guerre du Prophète Muhammed ?",
+        answer: "Bataille de Badr",
+        options: [
+            "Bataille de Uhud",
+            "Bataille de the Trench",
+            "Bataille de Badr",
+            "Conquête de la Mecque"
+        ]
+    },
+    {
+        numb: 9,
+        question: "Quel âge avait le prophète lorsque la révélation lui est tombée dessus ?",
+        answer: "40 ans",
+        options: [
+            "23 ans",
+            "40 ans",
+            "63 ans",
+            "43 ans"
+        ]
+    },
+    {
+        numb: 10,
+        question: "Quand a eu lieu la conquête de la Mecque ?",
+        answer: "8 hijri",
+        options: [
+            "8 hijri",
+            "12 hijri",
+            "6 hijri",
+            "9 hijri"
+        ]
+    }
+];
---
+++ questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What is the birth date of Prophet Muhammad?",
+        answer: "Abdullah ibn Abd al-Muttalib and Amina bint Wahb",
+        options: [
+            "Hashim ibn Abd Manaf and Salma bint Amr",
+            "Abdullah ibn Abd al-Muttalib and Amina bint Wahb",
+            "Abu Talib and Fatima bint Asad",
+            "Abdul Muttalib and Halimah bint Abi Dhuayb"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What is the significance of the city of Mecca in Prophet Muhammad’s life?",
+        answer: "It is where he was born and started his prophetic mission.",
+        options: [
+            "It is where he was born and started his prophetic mission.",
+            "It is where he migrated to escape persecution.",
+            "It is where he received his first revelation",
+            "It is where he wrote the Quran."
+        ]
+    },
+    {
+        numb: 3,
+        question: "Who was the first wife of Prophet Muhammad?",
+        answer: "Khadija bint Khuwaylid",
+        options: [
+            " Aisha bint Abi Bakr",
+            "Khadija bint Khuwaylid",
+            "Sawda bint Zam’a",
+            "Hafsa bint Umar"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What is the significance of the Hijra in the life of Prophet Muhammad?",
+        answer: "It marks his journey from Mecca to Medina.",
+        options: [
+            "It marks his journey from Medina to Mecca.",
+            "It marks his journey from Medina to Jerusalem.",
+            "It marks his journey from Mecca to Jerusalem.",
+            "It marks his journey from Mecca to Medina."
+        ]
+    },
+    {
+        numb: 5,
+        question: "What is the full name of Prophet Muhammed?",
+        answer: "Muhammad ibn Abdullah ibn Abdul-Muttalib",
+        options: [
+            "Muhammad ibn Ali ibn Abu Talib",
+            "Muhammad ibn Umar ibn Khattab",
+            "Muhammad ibn Abu Bakr ibn Uthman",
+            "Muhammad ibn Abdullah ibn Abdul-Muttalib"
+        ]
+    },
+    {
+        numb: 6,
+        question: "What was the name of Prophet Muhammad’s nursing mother?",
+        answer: "Halimah bint Abi Dhuayb",
+        options: [
+            " Khadijah bint Khuwaylid",
+            "Amina bint Wahb",
+            "Fatimah bint Muhammad",
+            "Halimah bint Abi Dhuayb"
+        ]
+    },
+    {
+        numb: 7,
+        question: "Which one of those is Prophet Muhammad's son?",
+        answer: "Qasim",
+        options: [
+            "Qasim",
+            "Mohamed",
+            "Issa",
+            "Hussein"
+        ]
+    },
+    {
+        numb: 8,
+        question: "What was the first war of Prophet Muhammed?",
+        answer: "Battle of Badr",
+        options: [
+            "Battle of Uhud",
+            "Battle of the Trench",
+            "Battle of Badr",
+            "Conquest of Mecca"
+        ]
+    },
+    {
+        numb: 9,
+        question: "How old was the prophet when the revelation came down on him?",
+        answer: "40 years old",
+        options: [
+            "23 years old",
+            "40 years old",
+            "63 years old",
+            "43 years old"
+        ]
+    },
+    {
+        numb: 10,
+        question: "When did the Conquest of Mecca took place?",
+        answer: "8th hijri",
+        options: [
+            "8th hijri",
+            "12 hijri",
+            "6 hijri",
+            "9 hijri"
+        ]
+    }
+];
